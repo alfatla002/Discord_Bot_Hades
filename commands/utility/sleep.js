ca429e2d8f520f8d21dec6fcd8ccd88c740f413f
alfatla002 <alfatla002@gannon.edu>
2026-02-19 05:52:19 -0500

    fixed user permissions

--- commands/utility/sleep.js
+++ commands/utility/sleep.js
@@ -1,11 +1,17 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('sleep')
-        .setDescription('Shut down Hades the chatbot completely'),
+        .setDescription('Shut down Hades the chatbot completely')
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
     
     async execute(interaction) {
+        if (!interaction.memberPermissions?.has(PermissionFlagsBits.Administrator)) {
+            await interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+            return;
+        }
+
         await interaction.reply('🐾 Hades is going to sleep... Goodbye!');
         
         // Wait for the reply to be sent before terminating the process
